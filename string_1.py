699170f5d5467d2ae4dcc3af913f046802881fbc
Dulhara Lakshan <100070252+dulhara19@users.noreply.github.com>
2024-06-24 03:29:36 +0530

    added string manipulation

---
+++ string_1.py
@@ -0,0 +1,20 @@
+# string indexing--------------------
+
+new_name= "Lakshan dulhara"
+print(new_name[0]) # L
+print(new_name[2:5]) #ksh
+print(new_name[0]+new_name[8]) # Ld
+
+print("L" in new_name)
+print("l" in new_name)
+print("f" not in new_name)
+print("s" not in new_name)
+print("dul" in new_name)
+
+
+s1= "python"
+s2= "programming"
+print( s1+" "+s2)
+
+# string  
+
