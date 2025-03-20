f4edc79537cfd0cc09ed4026d479cf4672860f66
dulhara <wkldulhara@gmail.com>
2025-03-20 23:49:03 +0530

    added more obj

--- json.py
+++ json.py
@@ -4,3 +4,8 @@ book ['book1'] = {
     "title": "The Hitchhiker's Guide to the Galaxy",    
     "author": "Douglas Adams",
     "price": 12.50}
+
+book ['book2'] = {
+    "title": "The Lord of the Rings",
+    "author": "J.R.R. Tolkien",
+    "price": 29.99}
