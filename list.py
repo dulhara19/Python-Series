e074ad7875db7a55176005be8a66a35fc5dfdef0
dulhara <wkldulhara@gmail.com>
2025-03-23 00:58:46 +0530

    upgraded

--- list.py
+++ list.py
@@ -13,3 +13,13 @@ print(food.count("bread"))  # Count number of times item appears in list
 
 food.insert(0, "grapes")  # Insert item at index
 print(food)
+
+food.sort()  # Sort list
+print(food)
+
+food.reverse()  # Reverse list
+print(food) 
+
+fruits = ["apple", "orange", "grapes"]
+food.extend(fruits)  # Add list to list
+print(food)
