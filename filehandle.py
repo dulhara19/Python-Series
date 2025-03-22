3d8f9493eba63d3b5793ef2f6b3e8eeac537b524
dulhara <wkldulhara@gmail.com>
2025-03-22 12:04:01 +0530

    write new file and count words

--- filehandle.py
+++ filehandle.py
@@ -1,5 +1,7 @@
 f=open("C:\\AI-projects\\new.txt","r+")
+fnew=open("C:\\AI-projects\\new1.txt","w+")
 
+#read() method is used to read the content of the file
 #if i want to append text to the file then use "a" instead of "w"
 #f=open("C:\\AI-projects\\new.txt","a")
 
@@ -15,10 +17,14 @@ f=open("C:\\AI-projects\\new.txt","r+")
 
 
 for line in f:
-    tokens = line.split('o') #split the line by letter 'o'
-    print(tokens)
-    print(len(tokens)) #print the length of the tokens
-    print(tokens[0]) #print the first token
+    #tokens = line.split('o') #split the line by letter 'o'
+    tokens = line.split(' ') #split the line by space
+    #print(tokens)
+    #print(len(tokens)) #print the length of the tokens
+    #print(tokens[0]) #print the first token
+
+    fnew.write(line + " word count :" + str(len(tokens)) + "\n") #write the line to the file
 
 #close the file
 f.close() 
+fnew.close()
